601a74cdf6db178a167785eb3d3266c9b796a8aa
bengfp <joann@LAPTOP-TAO4NKU9>
2023-06-13 06:02:51 -0400

    BiddingPage

--- SpeedBids.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SpeedBids.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ SpeedBids.WebUI/Controllers/BidController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpeedBids.Data;
+using SpeedBids.WebUI.Models;
+
+namespace SpeedBids.WebUI.Controllers
+{
+    public class BidController : Controller
+    {
+        private readonly SpeedBidsDbContext _dbContext;
+
+        public BidController(SpeedBidsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index(int productId)
+        {
+            var product = _dbContext.Products.FirstOrDefault(p => p.ProductId == productId);
+
+            var bidViewModel = new BidViewModel
+            {
+                ProductId = productId,
+                CarMake = product.CarMake,
+                CarModel = product.CarModel,
+                CarType = product.CarType,
+                Color = product.Color,
+                YearModel = product.YearModel,
+                ProductPrice = product.ProductPrice,
+                ProductImage = product.ProductImage
+
+            };
+
+            return View(bidViewModel);
+        }
+
+    }
+}
