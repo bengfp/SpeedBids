0cc5f5b8a9d4db2b29e4e3808be1cf70fa2907e2
bengfp <joann@LAPTOP-TAO4NKU9>
2023-06-13 03:23:21 -0400

    HomePageUpdate

--- SpeedBids.WebUI/Controllers/HomeController.cs
+++ SpeedBids.WebUI/Controllers/HomeController.cs
@@ -6,35 +6,41 @@ using System.Diagnostics;
 
 namespace SpeedBids.WebUI.Controllers
 {
-	public class HomeController : Controller
-	{
-		/*private readonly ILogger<HomeController> _logger;*/
-		private readonly SpeedBidsDbContext _dbContext;
+    public class HomeController : Controller
+    {
+        /*private readonly ILogger<HomeController> _logger;*/
+        private readonly SpeedBidsDbContext _dbContext;
 
-		/*public HomeController(ILogger<HomeController> logger)
+        /*public HomeController(ILogger<HomeController> logger)
 		{
 			_logger = logger;
 		}*/
-		public HomeController(SpeedBidsDbContext dbContext)
-		{
-			_dbContext = dbContext;
-		}
+        public HomeController(SpeedBidsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
-		public IActionResult Index()
-		{
-			List<Product> products = _dbContext.Products.ToList();
-			return View(products);
-		}
+        public IActionResult Index()
+        {
+            List<Product> products = _dbContext.Products.ToList();
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+            return View(products);
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
