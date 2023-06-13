addaac61b9a719dd430782553067b2be19ee3bf1
bengfp <joann@LAPTOP-TAO4NKU9>
2023-06-12 23:14:46 -0400

    HomePage

--- SpeedBids.WebUI/Controllers/HomeController.cs
+++ SpeedBids.WebUI/Controllers/HomeController.cs
@@ -1,32 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SpeedBids.Data;
+using SpeedBids.Entities;
 using SpeedBids.WebUI.Models;
 using System.Diagnostics;
 
 namespace SpeedBids.WebUI.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		/*private readonly ILogger<HomeController> _logger;*/
+		private readonly SpeedBidsDbContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		/*public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}*/
+		public HomeController(SpeedBidsDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			List<Product> products = _dbContext.Products.ToList();
+			return View(products);
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
